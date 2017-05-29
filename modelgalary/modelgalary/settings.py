7ef4be8f300a632b7caed2660bf8c6f9fb205df9
gmlove <gracekinglau@gmail.com>
2017-05-29 17:04:03 +0800

    disable django's host check

--- modelgalary/modelgalary/settings.py
+++ modelgalary/modelgalary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(lz5+^5igzh(r_)x6r)=so@rfn=wgs^!!=b^$&%@(zhntt&ifx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
